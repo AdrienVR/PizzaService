08216b1589980f5695c37b8a7c7aa3f92abb02f0
Dkapps <corentin.demay@laposte.net>
2014-12-09 16:10:54 +0100

    ajout d'un panier

--- Client_salle/app/js/controllers.js
+++ Client_salle/app/js/controllers.js
@@ -9,6 +9,28 @@ angular
     $scope.pizzas = Pizza.query();
 
     $scope.orderProp = 'note';
+
+    $scope.panier=[];
+
+    //$scope.index=0;
+
+    $scope.addPizza=function(pizza,comment,number){
+      var local=$scope.$new(true);
+      console.log(pizza);
+      console.log(comment);
+      //pizza.comment=comment;
+      local.pizza=angular.fromJson(pizza);
+
+      //local.pizza=local.pizza.concat(pizza);
+      //local.pizza.comment=comment;
+      $scope.panier=$scope.panier.concat(local.pizza);
+      console.log($scope.panier);
+    }
+
+    /*$scope.removePizza=function(pizza){
+      $scope.panier.pop()
+      console.log($scope.panier);
+    }*/
   }])
   .controller('PizzaDetailCtrl', ['$scope', '$routeParams', 'Pizza', function($scope, $routeParams, Pizza) {
 
